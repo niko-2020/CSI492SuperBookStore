287637fc2216dd980b539f2e3b69ce4025281ac2
nikomnw <nikomnw@gmail.com>
2023-05-03 22:26:39 -0700

    Add project files.

---
+++ CRWBookStore/Controllers/OrderController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CRWBookStore.Models;
+using System.Linq;
+namespace CRWBookStore.Controllers
+{
+    public class OrderController : Controller
+    {
+        private IOrderRepository repository;
+        private Cart cart;
+
+        public OrderController(IOrderRepository repoService, Cart cartService)
+        {
+            repository = repoService;
+            cart = cartService;
+        }
+
+        [HttpGet]
+        public ViewResult Checkout()
+        {
+            return View(new Order());
+        }
+
+        [HttpPost]
+        public IActionResult Checkout(Order order)
+        {
+            if (cart.Lines.Count() == 0)
+            {
+                ModelState.AddModelError("", "Sorry, your cart is empty!");
+            }
+            if (ModelState.IsValid)
+            {
+                order.Lines = cart.Lines.ToArray();
+                cart.Clear();
+                return RedirectToPage("/ThankYou", new { orderId = order.OrderID });
+            }
+            else
+            {
+                return View();
+            }
+        }
+        public IActionResult PaymentInfo()
+        {
+            return View();
+        }
+
+        /*
+        [HttpPost]
+        public IActionResult PaymentInfo()
+        {
+
+        }*/
+
+    }
+}
---
+++ CRWBookStore/Models/Order.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace CRWBookStore.Models
+{
+    public class Order
+    {
+
+        [BindNever]
+        public int OrderID { get; set; }
+        [BindNever]
+        public ICollection<CartLine> Lines { get; set; }
+
+        [Required(ErrorMessage = "Please enter a name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter the first address line")]
+        public string Line1 { get; set; }
+        public string Line2 { get; set; }
+        public string Line3 { get; set; }
+
+        [Required(ErrorMessage = "Please enter a city name")]
+        public string City { get; set; }
+
+        [StringLength(100)]
+        [Required(ErrorMessage = "Please enter a state name")]
+        public string State { get; set; }
+
+        [StringLength(5)]
+        [Required(ErrorMessage = "Please enter a zipcode")]
+        public string Zip { get; set; }
+
+        [Required(ErrorMessage = "Please enter a country name")]
+        public string Country { get; set; }
+
+        public bool GiftWrap { get; set; }
+    }
+}
