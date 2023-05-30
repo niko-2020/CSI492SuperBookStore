2436bbfd70f51eef3ff033bd6ddd731c2f4c9554
nikomnw <nikomnw@gmail.com>
2023-05-29 21:11:18 -0700

    Admin page and add category

---
+++ CRWBookStore/Controllers/addcategory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperBookStore.Controllers
+{
+    public class addcategory : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("addcategory");
+        }
+
+        public IActionResult AddCategory()
+        {
+            return View("addcategory");
+        }
+    }
+}
